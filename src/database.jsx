e1f677aa7895bbfb70df1c5f571e5fb341f8e060
Julia Arrington <jarring.dev@gmail.com>
2025-12-17 17:16:33 -0700

    added a message log

--- src/database.jsx
+++ src/database.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState} from 'react';
 class Customer {
   constructor(customerDB) {
     this.customerDB = customerDB;
@@ -11,28 +11,28 @@ class Customer {
    * Remove all rows from the database
    * @memberof Customer
    */
-  removeAllRows = () => {
+  removeAllRows = (addToLog) => {
     const request = indexedDB.open(this.customerDB, 1);
 
     request.onerror = (event) => {
-      console.log('removeAllRows - Database error: ', event.target.error.code,
+      addToLog('removeAllRows - Database error: ', event.target.error.code,
         " - ", event.target.error.message);
     };
 
     request.onsuccess = (event) => {
-      console.log('Deleting all customers...');
+      addToLog('Deleting all customers...');
       const db = event.target.result;
       const txn = db.transaction('customers', 'readwrite');
       txn.onerror = (event) => {
-        console.log('removeAllRows - Txn error: ', event.target.error.code,
+        addToLog('removeAllRows - Txn error: ', event.target.error.code,
           " - ", event.target.error.message);
       };
-      txn.oncomplete = (event) => {
-        console.log('All rows removed!');
+      txn.oncomplete = (addToLog) => {
+        addToLog('All rows removed!');
       };
       const objectStore = txn.objectStore('customers');
       const getAllKeysRequest = objectStore.getAllKeys();
-      getAllKeysRequest.onsuccess = (event) => {
+      getAllKeysRequest.onsuccess = () => {
         getAllKeysRequest.result.forEach(key => {
           objectStore.delete(key);
         });
@@ -45,20 +45,20 @@ class Customer {
    * @param {[object]} customerData Data to add
    * @memberof Customer
    */
-  initialLoad = (customerData) => {
+  initialLoad = (customerData, addToLog) => {
     const request = indexedDB.open(this.customerDB, 1);
 
     request.onerror = (event) => {
-      console.log('initialLoad - Database error: ', event.target.error.code,
+      addToLog('initialLoad - Database error: ', event.target.error.code,
         " - ", event.target.error.message);
     };
 
     request.onupgradeneeded = (event) => {
-      console.log('Populating customers...');
+      addToLog('Populating customers...');
       const db = event.target.result;
       const objectStore = db.createObjectStore('customers', { keyPath: 'userid' });
       objectStore.onerror = (event) => {
-        console.log('initialLoad - objectStore error: ', event.target.error.code,
+        addToLog('initialLoad - objectStore error: ', event.target.error.code,
           " - ", event.target.error.message);
       };
 
@@ -82,18 +82,17 @@ const customerDB = 'customer_db';
 /**
  * Clear all customer data from the database
  */
-const clearDB = (setStatus) => {
-  setStatus('Delete all rows from the Customers database');
+const clearDB = (addToLog) => {
+  addToLog('Delete all rows from the Customers database');
   let customer = new Customer(customerDB);
-  customer.removeAllRows();
+  customer.removeAllRows(addToLog);
 }
 
 /**
  * Add customer data to the database
  */
-const loadDB = (setStatus) => {
-  console.log('Load the Customers database');
-  setStatus('Loading database...');
+const loadDB = (addToLog) => {
+  addToLog('Loading database...');
 
   // Customers to add to initially populate the database with
   const customerData = [
@@ -101,11 +100,11 @@ const loadDB = (setStatus) => {
     { userid: '555', fname: 'Gilda', lname: 'Young', number: '4738572983' }
   ];
   let customer = new Customer(customerDB);
-  customer.initialLoad(customerData);
-  setStatus('Database loaded');
+  customer.initialLoad(customerData, addToLog);
+  addToLog('Database loaded');
 }
 //list all entries in the database
-const queryDB = (setStatus) => {
+const queryDB = (addToLog) => {
   const request = indexedDB.open('customer_db', 1);
 
   request.onerror = (event) => {
@@ -114,7 +113,7 @@ const queryDB = (setStatus) => {
   };
 
   request.onsuccess = (event) => {
-    setStatus('Querying database...');
+    addToLog('Querying database...');
     const db = event.target.result;
     const txn = db.transaction('customers', 'readonly');
     txn.onerror = (event) => {
@@ -125,7 +124,7 @@ const queryDB = (setStatus) => {
     const getAllRequest = objectStore.getAll();
     getAllRequest.onsuccess = () => {
       const customers = getAllRequest.result;
-      setStatus('Got all customers');
+      addToLog('Got all customers');
       console.table(customers);
     };
     getAllRequest.onerror = (err) => {
@@ -138,14 +137,22 @@ const queryDB = (setStatus) => {
 
 function Database() {
 
-  const [status, setStatus] = useState('Started');
+  const [status, setStatus] = useState(['Started']);
+
+  const addToLog = (message) => {
+    setStatus(prevStatus => [...prevStatus, message]);
+  }
 
   return < div className="project">
     <h2>Customer Database</h2>
-    <span id="ErrorOutput">{status}</span>
-    <button onClick={() => loadDB(setStatus)}>Load</button>
-    <button onClick={() => queryDB(setStatus)}>Query</button>
-    <button onClick={() => clearDB(setStatus)}>Clear</button>
+    <div id="ErrorOutput">
+      {status.map((msg, index) => (
+    <div key={index}>{msg}</div>
+  ))}
+    </div>
+    <button onClick={() => loadDB(addToLog)}>Load</button>
+    <button onClick={() => queryDB(addToLog)}>Query</button>
+    <button onClick={() => clearDB(addToLog)}>Clear</button>
   </div>
 }
 export default Database;
